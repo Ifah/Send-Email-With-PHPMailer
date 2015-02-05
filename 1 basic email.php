3db6bf28277851de1e349d0cb75f44de7dfb55dc
Mohamed Ifah <ifaa.x.h@gmail.com>
2015-02-06 00:09:32 +0500

    Send Email With PHPMailer
    Sending email in PHP, the right way.

---
+++ 1 basic email.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'libs/PHPMailer/PHPMailerAutoload.php';
+
+$m = new PHPMailer;
+
+$m->isSMTP();
+$m->SMTPAuth = true;
+$m->SMTPDebug = 2; //remove in production
+
+$m->Host = "smtp.gmail.com";
+$m->Username = 'example@gmail.com';
+$m->Password = 'password';
+$m->SMTPSecure = 'ssl';
+$m->Port = 465;
+
+
+$m->From = 'example@gmail.com';
+$m->FromName = 'Php Ifah';
+$m->addReplyTo('reply@gmail.com', 'Reply address');
+// $m->addAddress('example@gmail.com', 'Mohamed Ifah');
+$m->addAddress('example@gmail.com', 'Php Ifah');
+// $m->addAddress('example@gmail.com', 'ifaa x h');
+// $m->addCC('example@gmail.com', 'Mohamed Ifah');
+// $m->addBCC('example@gmail.com', 'Mohamed Ifah');
+
+$m->Subject = 'Test email';
+$m->Body = 'This is the body of an email!';
+$m->AltBody = 'This is the body of an email!';
+
+// var_dump($m->send());
+
+if($m->send()){
+	echo 'Email sent.';
+}else{
+	echo $m->ErrorInfo;
+}
+
+?>
